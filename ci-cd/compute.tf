a1de4d8f096a21e9701e335e9e292e488da9c1ae
Jean-Francois Royall <jfroyall2014@gmail.com>
2025-12-09 09:42:19 +0100

    This revision builds a simple network and a VM

--- ci-cd/compute.tf
+++ ci-cd/compute.tf
@@ -1,5 +1,66 @@
-#resource "libvirt_domain" "example" {
-#  name        = "example-vm"
+#
+# Virtual machine definition.
+resource "libvirt_domain" "alpine" {
+  name   = "alpine-vm"
+  memory = 1048576
+  vcpu   = 1
+
+  os = {
+    type    = "hvm"
+    type_arch    = "x86_64"
+    type_machine = "q35"
+  }
+
+  devices = {
+    disks = [
+      {
+        source = {
+          pool   = libvirt_volume.alpine_disk.pool
+          volume = libvirt_volume.alpine_disk.name
+        }
+        target = {
+          dev = "vda"
+          bus = "virtio"
+        }
+      },
+      {
+        device = "cdrom"
+        source = {
+          pool   = libvirt_volume.alpine_seed_volume.pool
+          volume = libvirt_volume.alpine_seed_volume.name
+        }
+        target = {
+          dev = "sda"
+          bus = "sata"
+        }
+      }
+    ]
+
+    interfaces = [
+      {
+        type  = "network"
+        model = "virtio"
+        source = {
+          network = "test-network"
+        }
+      }
+    ]
+
+    graphics = {
+      vnc = {
+        autoport = "yes"
+        listen   = "127.0.0.1"
+      }
+    }
+  }
+
+  running = true
+}
+
+
+
+#resource "libvirt_domain" "jumpbox" {
+#  name        = "jumpbox-vm"
 #  type        = "kvm"
 #  memory      = 512        # Flattened from <memory unit='MiB'>512</memory>
 #  #memory_unit = "MiB"      # Optional, defaults based on libvirt
@@ -26,4 +87,4 @@
 #    ]
 #  }
 #}
-
+#
--- ci-cd/resources.tf
+++ ci-cd/resources.tf
@@ -1,24 +1,128 @@
 # Basic pool
-#resource "libvirt_pool" "default" {
-#  name = "scratch-pool"
-#  type = "dir"
-#  source = {
-##    host = "localhost" 
-#    dir = "/scratch/scratch-pool" 
+resource "libvirt_pool" "default" {
+  name = "scratch-pool"
+  type = "dir"
+  source = {
+#    host = "localhost" 
+    dir = "/scratch/scratch-pool" 
+  }
+  target = {
+    path = "/scratch/scratch-pool"
+  }
+}
+
+# Volume from HTTP URL upload
+resource "libvirt_volume" "alpine_base" {
+  name   = "alpine-3.22.2.qcow2"
+  pool   = libvirt_pool.default.name
+  format = "qcow2"
+
+  create = {
+    content = {
+      url = "https://dl-cdn.alpinelinux.org/alpine/v3.22/releases/cloud/generic_alpine-3.22.2-x86_64-bios-cloudinit-r0.qcow2"
+    }
+  }
+  # capacity is automatically computed from Content-Length header
+}
+
+# Writable copy-on-write layer for the VM.
+resource "libvirt_volume" "alpine_disk" {
+  name     = "alpine-vm.qcow2"
+  pool     = libvirt_pool.default.name
+  format   = "qcow2"
+  capacity = 2147483648
+
+  backing_store = {
+    path   = libvirt_volume.alpine_base.path
+    format = "qcow2"
+  }
+}
+
+
+
+# Cloud-init seed ISO.
+resource "libvirt_cloudinit_disk" "alpine_seed" {
+  name = "alpine-cloudinit"
+
+  user_data = <<-EOF
+    #cloud-config
+    chpasswd:
+      list: |
+        root:password
+      expire: false
+
+    ssh_pwauth: true
+
+    packages:
+      - openssh-server
+
+    timezone: UTC
+  EOF
+
+  meta_data = <<-EOF
+    instance-id: alpine-001
+    local-hostname: alpine-vm
+  EOF
+
+  network_config = <<-EOF
+    version: 2
+    ethernets:
+      eth0:
+        dhcp4: true
+  EOF
+}
+
+# Upload the cloud-init ISO into the pool.
+resource "libvirt_volume" "alpine_seed_volume" {
+  name = "alpine-cloudinit.iso"
+  pool = "default"
+
+  create = {
+    content = {
+      url = libvirt_cloudinit_disk.alpine_seed.path
+    }
+  }
+}
+
+# A sub-network
+resource "libvirt_network" "test" {
+  name = "test-network"
+  autostart = "false"
+  ips = [ {
+    address = "192.168.96.0"
+    netmask = "255.255.255.0"
+#    prefix = 24
+#    dhcp= {
+#      hosts = {
+#        ip
+#      }
+#      ranges = []
+#    }
+  }
+  ]
+#
+#  dns = {
+#    enable = "true"
+#    #host   =
+#    #forward_plain_names   =
 #  }
-#  target = {
-#    path = "/scratch/scratch-pool"
+#  domain = {
+#    local_only = "false"
+#    name       = "nowhere.net"
+#
 #  }
-#}
+#  #forward = 
+}
+
 
 # Basic volume
-resource "libvirt_volume" "example" {
-  name = "example.qcow2"
-  #pool     = "${libvirt_pool.default}.name"
-  pool     = "default"
-  capacity = 1073741824 # 1 GB
-  format   = "qcow2"
-}
+#resource "libvirt_volume" "example" {
+#  name = "example.qcow2"
+#  #pool     = "${libvirt_pool.default}.name"
+#  pool     = "default"
+#  capacity = 1073741824 # 1 GB
+#  format   = "qcow2"
+#}
 #
 #resource "libvirt_volume" "vm-qcow2" {
 #  name   = "guest.qcow2"
@@ -73,3 +177,5 @@ resource "libvirt_volume" "example" {
 #  }
 #  # capacity is automatically computed from file size
 #}
+
+
