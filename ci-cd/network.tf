7786dc2acd6534a7eba3504a1151f91c26125e0b
Jean-Francois Royall <jfroyall2014@gmail.com>
2025-12-13 19:24:40 +0100

    Added the inner network

--- ci-cd/network.tf
+++ ci-cd/network.tf
@@ -1,4 +1,4 @@
-# A sub-network
+# The outer sub-network
 resource "libvirt_network" "outer" {
   name      = "outer-network"
   autostart = "false"
@@ -37,6 +37,81 @@ resource "libvirt_network" "outer" {
         {
           start = "192.168.96.128"
           end   = "192.168.96.191"
+        },
+      ]
+
+    }
+    }
+  ]
+
+  domain = {
+    local_only = "yes"
+    name       = "management.dabilly.home"
+    register   = "yes"
+  }
+
+  forward = {
+    #addresses = [{ }]
+
+    #dev = ""
+
+    #driver = { }
+
+    #interfaces = [{ }]
+
+    #managed = false
+
+    mode = "nat"
+
+    nat = {
+      port = {
+        start = 1024
+        end   = 65535
+      }
+    }
+
+  }
+}
+
+# The inner sub-network
+resource "libvirt_network" "inner" {
+  name      = "inner-network"
+  autostart = "false"
+
+  dns = {
+    enable = "yes"
+#    host   = [ { 
+#                  ip = "192.168.96.1"
+#                  hostnames = [
+#                    {
+#                      hostname = "papa"
+#                    }
+#                  ]
+#               }
+#             ]
+       #forward_plain_names   =
+  }
+
+  ips = [{
+    address = "192.168.97.0"
+    prefix  = 24
+    #netmask = "255.255.255.0"
+
+    ranges = [
+      {
+        start = "192.168.97.128"
+        end   = "192.168.97.191"
+      },
+    ]
+
+    dhcp = {
+      #      hosts = {
+      #        ip
+      #      }
+      ranges = [
+        {
+          start = "192.168.97.128"
+          end   = "192.168.97.191"
         },
       ]
 
