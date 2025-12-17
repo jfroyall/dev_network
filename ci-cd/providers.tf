414fc76f96162527875cfd68f8258f30887b4977
jean-francois royall <jean@mac.home>
2025-12-17 21:37:13 +0100

    Cosmetic changes

--- ci-cd/providers.tf
+++ ci-cd/providers.tf
@@ -2,6 +2,7 @@
 provider "libvirt" {
   # Configuration du fournisseur libvirt
   uri = "qemu+sshcmd://jean@192.168.1.96/system?keyfile=/home/jean/.ssh/id_rsa&no_verify=0&sshauth=privkey"
+  #uri = "qemu:///session"
 
 }
 
--- ci-cd/undefine.sh
+++ ci-cd/undefine.sh
@@ -1,6 +1,11 @@
+
+
+export VIRSH_DEFAULT_CONNECT_URI="qemu+ssh://jean@192.168.1.96/system?keyfile=/home/jean/.ssh/id_rsa&no_verify=0&sshauth=privkey"
+
 for d in ns1 jumpbox vault; do 
 echo $d; 
-  virsh -c qemu:///system shutdown $d && virsh -c qemu:///system undefine $d
+  virsh  shutdown $d  && virsh  undefine $d
+  #virsh -c qemu:///system shutdown $d && virsh -c qemu:///system undefine $d
 done
 #virsh -c qemu:///system shutdown alpine-vm-0 && virsh -c qemu:///system undefine alpine-vm-0
 #virsh -c qemu:///system shutdown alpine-vm-1 && virsh -c qemu:///system undefine alpine-vm-1
